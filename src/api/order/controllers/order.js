e448440bc8ac0b8950e2f253417eb0d0a747bd14
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-05 10:02:51 -0300

    📝 :pencil: [Minor] Create order - create costumer

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,17 +1,4 @@
-// 'use strict';
-
-// /**
-//  *  order controller
-//  */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-// module.exports = createCoreController('api::order.order');
-
-
-
-
-"use strict";
+'use strict';
 
 /**
  * Order.js controller
@@ -19,7 +6,11 @@ const { createCoreController } = require('@strapi/strapi').factories;
  * @description: A set of functions called "actions" for managing `Order`.
  */
 
-module.exports = createCoreController('api::order.order', ({ strapi }) =>  ({
+const { createCoreController } = require('@strapi/strapi').factories;
+
+const stripe = require('stripe')('sk_test_51KOP3uHbz4bukEWOWL8CLwqEzuF9kzvOqTFl6K1JzEqA9oTMRO8bBQa4LbdZ71UYw4eUDFKISuXd39Rb04tgd3bN00P7I1DioX');
+
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
   /**
    * Create a/an order record.
    *
@@ -27,30 +18,8 @@ module.exports = createCoreController('api::order.order', ({ strapi }) =>  ({
    */
 
   create: async (ctx) => {
-    const { address, amount, dishes, token, city, state } = JSON.parse(
-      ctx.request.body
-    );
-    const stripeAmount = Math.floor(amount * 100);
-    // charge on stripe
-    const charge = await stripe.charges.create({
-      // Transform cents to dollars.
-      amount: stripeAmount,
-      currency: "usd",
-      description: `Order ${new Date()} by ${ctx.state.user._id}`,
-      source: token,
+    const customer = await stripe.customers.create({
+      description: 'My First Test Customer',
     });
-
-    // Register the order in the database
-    const order = await strapi.services.order.create({
-      user: ctx.state.user.id,
-      charge_id: charge.id,
-      amount: stripeAmount,
-      address,
-      dishes,
-      city,
-      state,
-    });
-
-    return order;
-  },
+  }
 }))
