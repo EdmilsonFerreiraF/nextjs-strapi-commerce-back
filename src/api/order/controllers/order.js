71a6756b8da728190b00eb6813d2a76b76c904f5
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-11 23:31:34 -0300

    📝 :pencil: [Minor] Order - PagSeguro payment

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -7,35 +7,64 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
-
+const axios = require('axios')
 const stripe = require('stripe')('sk_test_51KOP3uHbz4bukEWOWL8CLwqEzuF9kzvOqTFl6K1JzEqA9oTMRO8bBQa4LbdZ71UYw4eUDFKISuXd39Rb04tgd3bN00P7I1DioX');
 
 module.exports = createCoreController('api::order.order', ({ strapi }) => ({
-  /**
-   * Create a/an order record.
-   *
-   * @return {Object}
-   */
-  createStripe: async (ctx) => {
-    const customer = await stripe.customers.create({
-      description: 'My First Test Customer',
-    });
+    /**
+     * Create a/an order record.
+     *
+     * @return {Object}
+     */
+    createStripe: async (ctx) => {
+        const {
+            description: { costumer: { description } },
+            currency,
+            amount,
+            payment_method_types,
+            setup_future_usage,
+        } = ctx.request.body
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      customer: customer.id,
-      currency: 'usd',
-      amount: 2000,
-      payment_method_types: ['card'],
-      setup_future_usage: 'on_session',
-    });
+        console.log('description', description)
 
-    return { clientSecret: paymentIntent.client_secret }
-  },
-  createPagSeguro: async (ctx) => {
-    try {
-      ctx.body = 'ok';
-    } catch (err) {
-      ctx.body = err;
-    }
-  },
+        const customer = await stripe.customers.create({
+            description
+        });
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            customer: customer.id,
+            currency,
+            amount,
+            payment_method_types,
+            setup_future_usage,
+        });
+
+        return { clientSecret: paymentIntent.client_secret }
+    },
+    createPagSeguro: async (ctx) => {
+        const token = "5D3179F168F94D11B1C9834A3C154139"
+
+        try {
+
+            const res = await axios.post('https://sandbox.api.pagseguro.com/orders',
+            ctx.request.body,
+            
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    },
+                },
+            )
+            .then(res => {
+                return res
+            })
+            .catch(err => console.log('err', err.response.data.error_messages))
+
+            return res
+        } catch (err) {
+            console.log('err', err)
+            ctx.body = err;
+        }
+    },
 }))
--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -8,32 +8,32 @@ module.exports = {
     routes: [
         { // Path defined with a regular expression
             method: 'POST',
-            path: '/order/stripe', // Only match when the first parameter contains 2 or 3 digits.
+            path: '/orders/stripe', // Only match when the first parameter contains 2 or 3 digits.
             handler: 'order.createStripe',
         },
         { // Path defined with a regular expression
             method: 'POST',
-            path: '/order/pagseguro', // Only match when the first parameter contains 2 or 3 digits.
+            path: '/orders/pagseguro', // Only match when the first parameter contains 2 or 3 digits.
             handler: 'order.createPagSeguro',
         },
         { // Path defined with a URL parameter
             method: 'GET',
-            path: '/order',
+            path: '/orders',
             handler: 'order.find',
         },
         { // Path defined with a URL parameter
             method: 'GET',
-            path: '/order/:id',
+            path: '/orders/:id',
             handler: 'order.findOne',
         },
         { // Path defined with a regular expression
             method: 'DELETE',
-            path: '/order/:id', // Only match when the first parameter contains 2 or 3 digits.
+            path: '/orders/:id', // Only match when the first parameter contains 2 or 3 digits.
             handler: 'order.delete',
         },
         { // Path defined with a regular expression
             method: 'PUT',
-            path: '/order/:id', // Only match when the first parameter contains 2 or 3 digits.
+            path: '/orders/:id', // Only match when the first parameter contains 2 or 3 digits.
             handler: 'order.update',
         },
     ]
